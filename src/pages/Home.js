c13de10331a2353a88b13f4a5a5229805c33b49a
Vishwajeet Kumar <vishwajeet7456@gmail.com>
2022-07-07 09:31:15 +0530

    created two more new hooks: useLastQuery and useShow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,17 @@ import MainPageLayout from '../components/MainPageLayout';
 import { apiGet } from '../misc/config';
 import ShowGrid from '../components/show/ShowGrid';
 import ActorGrid from '../components/actor/ActorGrid';
+import { useLastQuery } from '../misc/custom-hooks';
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery();
   const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
 
-  const isShowSearch = searchOption === 'shows';
-
+  const isShowsSearch = searchOption === 'shows';
   const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
-      console.log(results);
     });
   };
 
@@ -59,24 +58,24 @@ const Home = () => {
       />
 
       <div>
-        <label htmlFor="show-search">
-          shows
+        <label htmlFor="shows-search">
+          Shows
           <input
-            id="show-search"
+            id="shows-search"
             type="radio"
             value="shows"
-            checked={isShowSearch}
+            checked={isShowsSearch}
             onChange={onRadioChange}
           />
         </label>
 
-        <label htmlFor="actor-search">
+        <label htmlFor="actors-search">
           Actors
           <input
-            id="actor-search"
+            id="actors-search"
             type="radio"
             value="people"
-            checked={!isShowSearch}
+            checked={!isShowsSearch}
             onChange={onRadioChange}
           />
         </label>
@@ -85,7 +84,6 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
-
       {renderResults()}
     </MainPageLayout>
   );
